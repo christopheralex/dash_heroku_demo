923eb863ecb10bdb584e490bbf99dd4356c34c56
Christopher Alexander <calexa03@student.ubc.ca>
2022-02-17 01:02:04 -0800

    feat:Adds intital files

---
+++ app.py
@@ -0,0 +1,34 @@
+import altair as alt
+import pandas as pd
+from dash import Dash, dcc, html, Input, Output
+alt.data_transformers.enable('data_server')
+alt.renderers.enable('mimetype')
+
+    
+df = pd.read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv")
+
+
+def plot_altair(xmax, data=df.copy()):
+    chart = alt.Chart(data[data["track_popularity"] <= xmax]).mark_line().encode(
+        x=alt.X('track_popularity',  bin=alt.Bin(maxbins=20)),
+        y = "mean(liveness)")
+    return chart.to_html()
+
+app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+server = app.server
+
+app.layout = html.Div([
+        html.Iframe(
+            id='scatter',
+            srcDoc=plot_altair(xmax=0),
+            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+        dcc.Slider(id='xslider', min=0, max=100)])
+        
+@app.callback(
+    Output('scatter', 'srcDoc'),
+    Input('xslider', 'value'))
+def update_output(xmax):
+    return plot_altair(xmax)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
